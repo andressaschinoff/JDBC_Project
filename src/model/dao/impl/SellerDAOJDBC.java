dc35677fa26fdfbd3664d7d2bbac02b217976f6e
Andressa Schinoff <andressaschinoff@gmail.com>
2020-08-25 16:14:50 -0300

    create DAO findById

--- src/model/dao/impl/SellerDAOJDBC.java
+++ src/model/dao/impl/SellerDAOJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DBException;
 import model.dao.SellerDAO;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDAOJDBC implements SellerDAO{
+	
+	private Connection connection;
+	
+	public SellerDAOJDBC(Connection connection) {
+		this.connection = connection;
+	}
 
 	@Override
 	public void insert(Seller seller) {
@@ -27,8 +40,45 @@ public class SellerDAOJDBC implements SellerDAO{
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = connection.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " + 
+					"FROM seller INNER JOIN department " + 
+					"ON seller.DepartmentId = department.Id " + 
+					"WHERE seller.Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department depart = new Department();
+				
+				depart.setId(rs.getInt("DepartmentId"));
+				depart.setName(rs.getString("DepName"));
+				
+				Seller seller = new Seller();
+				
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBaseSalary(rs.getDouble("BaseSalary"));
+				seller.setBirthdate(rs.getDate("BirthDate"));
+				seller.setDepartment(depart);
+				
+				return seller;
+			}
+			
+			return null;
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -22,6 +22,10 @@ public class Department implements Serializable {
 		return id;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
