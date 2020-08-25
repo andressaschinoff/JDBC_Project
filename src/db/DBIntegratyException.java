80bc5d9167ad6390ce3eb604770fc00a031099fb
Andressa Schinoff <andressaschinoff@gmail.com>
2020-08-25 14:09:24 -0300

    template

---
+++ src/db/DBIntegratyException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DBIntegratyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DBIntegratyException(String msg) {
+		super(msg);
+	}
+}
