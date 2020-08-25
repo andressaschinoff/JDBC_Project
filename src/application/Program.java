6ed57967e703777d098cfc2270c6bade8ccc1287
Andressa Schinoff <andressaschinoff@gmail.com>
2020-08-25 14:54:43 -0300

    Department entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Department depart = new Department(1, "Books");
+		
+		System.out.println(depart);
 	}
 
 }
